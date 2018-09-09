4ac6f77bba419995dd8b6c6fa5698027f43b25a8
fubst0318 <fubst0318@126.com>
2018-09-10 07:33:09 +0800

    scrapy 继续学习

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+from tutorial.items import QuoteItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'  # 爬虫的名字，唯一
+    allowed_domains = ['qutoes.toscrape.com']
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.css('.quote')
+        for quote in quotes:
+            item = QuoteItem()
+            item['text'] = quote.css('.text::text').extract_first()
+            item['author'] = quote.css('.author::text').extract_first()
+            item['tags'] = quote.css('.tags .tag::text').extract()
+            yield item
+
+        next = response.css('.pager .next a::attr("href")').extract_first()
+        url = response.urljoin(next)
+        yield scrapy.Request(url, callback=self.parse)
+
+    # def start_requests(self):
+    #     urls = [
+    #         'http://quotes.toscrape.com/page/1/',
+    #         'http://quotes.toscrape.com/page/2/'
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+
+    # def parse(self, response):  # 其中的respnose 是 TextResponse的一个实例
+    #     page = response.url.split('/')[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
